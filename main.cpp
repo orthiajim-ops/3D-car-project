0461bff556807794779be88899b5851d7f8a5aa8
Huraira Jim <orthiajim@gmail.com>
2026-05-03 09:02:08 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,103 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"uniform vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(ourColor, 1.0f);\n"
+"}\n\0";
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void drawRect(unsigned int shaderProgram, float x, float y, float w, float h, float r, float g, float b) {
+    float vertices[] = {
+         x + w,  y + h, 0.0f,
+         x + w,  y,     0.0f,
+         x,      y,     0.0f,
+         x,      y + h, 0.0f
+    };
+    unsigned int indices[] = { 0, 1, 3, 1, 2, 3 };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+    glUseProgram(shaderProgram);
+    glUniform3f(vertexColorLocation, r, g, b);
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+}
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Final Car Project", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+  
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+        glClearColor(0.5f, 0.8f, 0.9f, 1.0f); 
+        glClear(GL_COLOR_BUFFER_BIT);
+
+       
+        drawRect(shaderProgram, -1.0f, -0.8f, 2.0f, 0.5f, 0.2f, 0.2f, 0.2f);
+        
+        drawRect(shaderProgram, -0.8f, -0.3f, 0.4f, 0.8f, 0.4f, 0.4f, 0.4f);
+       
+        drawRect(shaderProgram, 0.4f, -0.3f, 0.4f, 1.0f, 0.3f, 0.3f, 0.3f);
+        
+        drawRect(shaderProgram, -0.25f, -0.6f, 0.5f, 0.2f, 1.0f, 0.0f, 0.0f);
+        
+        drawRect(shaderProgram, -0.15f, -0.4f, 0.3f, 0.2f, 0.0f, 0.0f, 0.0f);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
+}
